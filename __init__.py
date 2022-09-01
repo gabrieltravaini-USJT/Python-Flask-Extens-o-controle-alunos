70baef840ee2c6c35a7f86a6904bec86fdeaa36c
Gabriel Travaini <gabrieltravaini.8039@aluno.saojudas.br>
2022-08-31 22:19:28 -0300

    created test on init.py hello world

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask 
 
 def create_app():
-    app = Flask (__name__)
+    app = Flask(__name__)
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
 
     return app
